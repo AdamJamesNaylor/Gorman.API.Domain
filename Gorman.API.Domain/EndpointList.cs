5a605f09e64ab2aab89237ad5e4e74474422a80f
Adam Naylor <adamnaylor@gmail.com>
2016-09-12 17:21:53 +0100

    Moved to response for endpoints

---
+++ Gorman.API.Domain/EndpointList.cs
@@ -0,0 +1,17 @@
+﻿namespace Gorman.API.Domain {
+    using Newtonsoft.Json;
+
+    public class EndpointList {
+        [JsonProperty("maps_url")]
+        public string MapsUrl { get; set; }
+
+        [JsonProperty("activities_url")]
+        public string ActivitiesUrl { get; set; }
+
+        [JsonProperty("actors_url")]
+        public string ActorsUrl { get; set; }
+
+        [JsonProperty("actions_url")]
+        public string ActionsUrl { get; set; }
+    }
+}
--- Gorman.API.Domain/Response.cs
+++ Gorman.API.Domain/Response.cs
@@ -3,20 +3,6 @@ namespace Gorman.API.Domain {
     using System.Collections.ObjectModel;
     using Newtonsoft.Json;
 
-    public class EndpointList {
-        [JsonProperty("maps_url")]
-        public string MapsUrl { get; set; }
-
-        [JsonProperty("activities_url")]
-        public string ActivitiesUrl { get; set; }
-
-        [JsonProperty("actors_url")]
-        public string ActorsUrl { get; set; }
-
-        [JsonProperty("actions_url")]
-        public string ActionsUrl { get; set; }
-    }
-
     public class Response<T> {
         public T Data { get; set; }
         public ErrorResponse Error { get; set; }
